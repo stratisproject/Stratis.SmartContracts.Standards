ee48d62cdaf4c70b38e51fd19f0d5b1ee44a5d2e
quantumagi <someguy.fromafrica@gmail.com>
2021-01-12 16:18:08 +1100

    Use uint256

--- Stratis.SmartContracts.Standards/IStandardToken.cs
+++ Stratis.SmartContracts.Standards/IStandardToken.cs
@@ -1,4 +1,6 @@
-﻿namespace Stratis.SmartContracts.Standards
+﻿using Stratis.SmartContracts;
+
+namespace Stratis.SmartContracts.Standards
 {
     /// <summary>
     /// Interface for a standard smart contract token
@@ -8,14 +10,14 @@
         /// <summary>
         /// The total number of tokens in existence.
         /// </summary>
-        ulong TotalSupply { get; }
+        uint256 TotalSupply { get; }
 
         /// <summary>
         /// Gets the balance of the specified address.
         /// </summary>
         /// <param name="address">The address to query the balance of.</param>
         /// <returns>Balance for the given address</returns>
-        ulong GetBalance(Address address);
+        uint256 GetBalance(Address address);
 
         /// <summary>
         /// Transfers tokens owned by the sender to the destination address.
@@ -23,7 +25,7 @@
         /// <param name="to">The address to transfer the tokens to.</param>
         /// <param name="amount">The amount of tokens to transfer.</param>
         /// <returns>A bool representing whether the transfer was successful.</returns>
-        bool TransferTo(Address to, ulong amount);
+        bool TransferTo(Address to, uint256 amount);
 
         /// <summary>
         /// Transfers tokens owned by one address to another address.
@@ -33,7 +35,7 @@
         /// <param name="to">The address to transfer the tokens to.</param>
         /// <param name="amount">The amount of tokens to transfer.</param>
         /// <returns>A bool representing whether the transfer was successful.</returns>
-        bool TransferFrom(Address from, Address to, ulong amount);
+        bool TransferFrom(Address from, Address to, uint256 amount);
 
         /// <summary>
         /// Approves the spender to be able to transfer an amount of tokens on Message.Sender's behalf.
@@ -42,7 +44,7 @@
         /// <param name="currentAmount">The current amount of tokens that the spender is approved to transfer.</param>
         /// <param name="amount">To amount of tokens to approve.</param>
         /// <returns>A bool representing whether the approval was successful.</returns>
-        bool Approve(Address spender, ulong currentAmount, ulong amount);
+        bool Approve(Address spender, uint256 currentAmount, uint256 amount);
 
         /// <summary>
         /// Returns the amount of tokens owned by owner that the spender is able to spend.
@@ -50,6 +52,6 @@
         /// <param name="owner">The address of the owner of the tokens.</param>
         /// <param name="spender">The address of the spender of the tokens.</param>
         /// <returns>The number of tokens.</returns>
-        ulong Allowance(Address owner, Address spender);
+        uint256 Allowance(Address owner, Address spender);
     }
 }
